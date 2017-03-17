258d6b50453115ce98e578aead9cedfe64d3912b
程序小白猿 <15301072780@163.com>
2017-03-17 18:17:20 +0800

    模拟输入信息和点击按钮

--- spider.py
+++ spider.py
@@ -12,21 +12,30 @@ wait = WebDriverWait(browser, 10)
 
 
 def search():
-    browser.get("https://wwww.taobao.com")
+    browser.get("https://s.taobao.com")
     # selenium处理页面等待的方法
     # 设置一下需要监听的元素，可以通过id或者能唯一确定元素的属性
     # EC是选择条件点后边的是条件，我把条件记录在简书里
 
-    try:
-        # 我们通过页面元素，查找到输入框，我们利用css样式去确定，并利用css选择器去获取
-        element = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "#q"))
-        )
+    # 我们通过页面元素，查找到输入框，我们利用css样式去确定，并利用css选择器去获取
+    input = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#q"))
+    )
 
-        # 然后监听按钮，条件是以可以点击判断其加载完成
-        submit = wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#J_SearchForm > button"))
-        )
-    finally:
-        browser.quit()
+    # 然后监听按钮，条件是以可以点击判断其加载完成
+    # #J_SearchForm > div > div.search-button > button
+    # J_SearchForm > div > div.search-button > button
+    submit = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "#J_SearchForm > div > div.search-button > button"))
+    )
 
+    # 调用selenium的API来给搜索框输入内容，然后按钮追加点击方法
+    keyword = "美食".decode(encoding="utf-8")
+    input.send_keys(keyword)
+    submit.click()
+
+def main():
+    search()
+
+if __name__ == "__main__":
+    main()
